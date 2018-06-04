359a069572e65b21d8079b502f851ecdca195aa8
hiyosuke <39537559+hiyosuke@users.noreply.github.com>
2018-06-04 17:14:37 -0400

    Add files via upload

---
+++ routes/index.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const router = express.Router();
+//const request = require('request');
+
+router.route('/:name')
+
+.get (function (req, res, next) {
+    req.params.name = {
+        string: `${req.params.name}`,
+        length: req.params.name.length
+    }
+    res.json(req.params.name)
+})
+
+router.route('/')
+
+.post (function (req, res, next) {
+    let name  = {
+        string: req.body.string,
+        length: req.body.string.length
+    }
+    res.json(name)
+})
+
+module.exports = router;
